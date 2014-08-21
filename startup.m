64c1c47ce7ce32e653a44313eae2d634162259fd
Jan Hosang <jan.hosang@gmail.com>
2014-08-21 21:27:26 +0200

    made startup.m more robust

--- startup.m
+++ startup.m
@@ -1,7 +1,9 @@
 
 if ~isdeployed()
-  addpath('repeatability');
-  addpath('util');
-  addpath('shared');
-  addpath('recall');
+  proposal_root_path = fileparts(mfilename('fullpath'));
+  
+  addpath(fullfile(proposal_root_path, 'repeatability'));
+  addpath(fullfile(proposal_root_path, 'util'));
+  addpath(fullfile(proposal_root_path, 'shared'));
+  addpath(fullfile(proposal_root_path, 'recall'));
 end
