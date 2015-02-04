d8285461eec9d373b941a8823cda734fadb73601
Jan Hosang <jan.hosang@gmail.com>
2015-02-04 11:47:41 +0100

    added new methods

--- shared/get_method_configs.m
+++ shared/get_method_configs.m
@@ -5,23 +5,31 @@ function methods = get_method_configs()
 
 % If you want to add your own method, add it at the bottom.
 
-colormap = [ ...
-228, 229, 97 ; ...
-163, 163, 163 ; ...
-218, 71, 56 ; ...
-219, 135, 45 ; ...
-145, 92, 146 ; ...
-83, 136, 173 ; ...
-135, 130, 174 ; ...
-225, 119, 174 ; ...
-142, 195, 129 ; ...
-138, 180, 66 ; ...
-223, 200, 51 ; ...
-92, 172, 158 ; ...
-177,89,40;
-0, 255, 255;
-188, 128, 189;
-255, 255, 0;
+colormap = [
+228, 229, 97
+163, 163, 163
+218, 71, 56
+219, 135, 45
+145, 92, 146
+83, 136, 173
+106,61,154
+225, 119, 174
+142, 195, 129
+51,160,44
+223, 200, 51
+92, 172, 158
+177,89,40
+177,89,40
+188, 128, 189
+177,89,40
+251,154,153
+31,120,180
+177,89,40
+177,89,40
+177,89,40
+177,89,40
+177,89,40
+177,89,40
 ] ./ 256;
 
   config = get_config();
@@ -60,7 +68,7 @@ colormap = [ ...
   methods(i).is_baseline = false;
   
   i = numel(methods) + 1;
-  methods(i).name = 'Sel.Search';
+  methods(i).name = 'SelectiveSearch';
   methods(i).short_name = 'SS';
   prefix = [precomputed_prefix 'selective_search/'];
   methods(i).candidate_dir = [prefix 'mat'];
@@ -75,7 +83,7 @@ colormap = [ ...
   methods(i).is_baseline = false;
 
   i = numel(methods) + 1;
-  methods(i).name = 'Rand.Prim';
+  methods(i).name = 'RandomizedPrims';
   methods(i).short_name = 'RP';
   prefix = [precomputed_prefix 'randomized_prims/'];
   methods(i).candidate_dir = [prefix 'mat'];
@@ -170,7 +178,7 @@ colormap = [ ...
   methods(i).is_baseline = false;
   
   i = numel(methods) + 1;
-  methods(i).name = 'Ranta.2014';
+  methods(i).name = 'Rantalankila';
   methods(i).short_name = 'R4';
   prefix = [precomputed_prefix 'Rantalankila/'];
   methods(i).candidate_dir = [prefix 'mat'];
@@ -218,7 +226,7 @@ colormap = [ ...
   
   i = numel(methods) + 1;
   methods(i).name = 'EdgeBoxes';
-  methods(i).short_name = 'EB';
+  methods(i).short_name = 'EB70';
   prefix = [precomputed_prefix 'edge_boxes_70/'];
   methods(i).candidate_dir = [prefix 'mat'];
   methods(i).repeatability_candidate_dir = [prefix 'repeatability_mat'];
@@ -232,7 +240,7 @@ colormap = [ ...
   methods(i).is_baseline = false;
   
   i = numel(methods) + 1;
-  methods(i).name = 'Edge Boxes 50';
+  methods(i).name = 'EdgeBoxes50';
   methods(i).short_name = 'EB50';
   prefix = [precomputed_prefix 'edge_boxes_50/'];
   methods(i).candidate_dir = [prefix 'mat'];
@@ -245,6 +253,7 @@ colormap = [ ...
   methods(i).num_candidates = true;
   methods(i).color = colormap(i,:);
   methods(i).is_baseline = false;
+  methods(i).line_style = ':';
   
   i = numel(methods) + 1;
   methods(i).name = 'MCG';
@@ -262,7 +271,7 @@ colormap = [ ...
   methods(i).is_baseline = false;
   
   i = numel(methods) + 1;
-  methods(i).name = 'Edge Boxes 90';
+  methods(i).name = 'EdgeBoxes90';
   methods(i).short_name = 'EB90';
   prefix = [precomputed_prefix 'edge_boxes_90/'];
   methods(i).candidate_dir = [prefix 'mat'];
@@ -272,6 +281,129 @@ colormap = [ ...
   methods(i).repeatability_matching_file = [prefix 'repeatability_matching.mat'];
   methods(i).order = 'descend';
   methods(i).extract = @run_edge_boxes90;
+  methods(i).num_candidates = true;
+  methods(i).color = colormap(i,:);
+  methods(i).is_baseline = false;
+  methods(i).line_style = '-.';
+  
+  i = numel(methods) + 1;
+  methods(i).name = 'Rigor';
+  methods(i).short_name = 'Ri';
+  prefix = [precomputed_prefix 'rigor/'];
+  methods(i).candidate_dir = [prefix 'mat'];
+  methods(i).repeatability_candidate_dir = [prefix 'repeatability_mat'];
+  methods(i).best_voc07_candidates_file = [prefix 'best_candidates.mat'];
+  methods(i).best_imagenet_candidates_file = [prefix 'best_candidates_imagenet.mat'];
+  methods(i).repeatability_matching_file = [prefix 'repeatability_matching.mat'];
+  methods(i).order = 'none';
+  methods(i).extract = @run_rigor;
+  methods(i).num_candidates = true;
+  methods(i).color = colormap(i,:);
+  methods(i).is_baseline = false;
+  methods(i).rerun_num_candidates = [10 100 1000 2000 10000];
+
+  i = numel(methods) + 1;
+  methods(i).name = 'Geodesic';
+  methods(i).short_name = 'G';
+  prefix = [precomputed_prefix 'geodesic/'];
+  methods(i).candidate_dir = [prefix 'mat'];
+  methods(i).repeatability_candidate_dir = [prefix 'repeatability_mat'];
+  methods(i).best_voc07_candidates_file = [prefix 'best_candidates.mat'];
+  methods(i).best_imagenet_candidates_file = [prefix 'best_candidates_imagenet.mat'];
+  methods(i).repeatability_matching_file = [prefix 'repeatability_matching.mat'];
+  methods(i).order = 'none';
+  methods(i).extract = @run_gop;
+  methods(i).num_candidates = false;
+  methods(i).color = colormap(i,:);
+  methods(i).is_baseline = false;
+  methods(i).rerun_num_candidates = [10 100 1000 2000 10000];
+  
+  i = numel(methods) + 1;
+  methods(i).name = 'EdgeBoxes60';
+  methods(i).short_name = 'EB60';
+  prefix = [precomputed_prefix 'edge_boxes_60/'];
+  methods(i).candidate_dir = [prefix 'mat'];
+  methods(i).repeatability_candidate_dir = [prefix 'repeatability_mat'];
+  methods(i).best_voc07_candidates_file = [prefix 'best_candidates.mat'];
+  methods(i).best_imagenet_candidates_file = [prefix 'best_candidates_imagenet.mat'];
+  methods(i).repeatability_matching_file = [prefix 'repeatability_matching.mat'];
+  methods(i).order = 'descend';
+  methods(i).extract = @run_edge_boxes60;
+  methods(i).num_candidates = true;
+  methods(i).color = colormap(i,:);
+  methods(i).is_baseline = false;
+  
+  i = numel(methods) + 1;
+  methods(i).name = 'EdgeBoxes80';
+  methods(i).short_name = 'EB80';
+  prefix = [precomputed_prefix 'edge_boxes_80/'];
+  methods(i).candidate_dir = [prefix 'mat'];
+  methods(i).repeatability_candidate_dir = [prefix 'repeatability_mat'];
+  methods(i).best_voc07_candidates_file = [prefix 'best_candidates.mat'];
+  methods(i).best_imagenet_candidates_file = [prefix 'best_candidates_imagenet.mat'];
+  methods(i).repeatability_matching_file = [prefix 'repeatability_matching.mat'];
+  methods(i).order = 'descend';
+  methods(i).extract = @run_edge_boxes80;
+  methods(i).num_candidates = true;
+  methods(i).color = colormap(i,:);
+  methods(i).is_baseline = false;
+  
+  i = numel(methods) + 1;
+  methods(i).name = 'EdgeBoxes55';
+  methods(i).short_name = 'EB55';
+  prefix = [precomputed_prefix 'edge_boxes_55/'];
+  methods(i).candidate_dir = [prefix 'mat'];
+  methods(i).repeatability_candidate_dir = [prefix 'repeatability_mat'];
+  methods(i).best_voc07_candidates_file = [prefix 'best_candidates.mat'];
+  methods(i).best_imagenet_candidates_file = [prefix 'best_candidates_imagenet.mat'];
+  methods(i).repeatability_matching_file = [prefix 'repeatability_matching.mat'];
+  methods(i).order = 'descend';
+  methods(i).extract = @(im,num) run_edge_boxes(im, num, 0.65, 0.60);
+  methods(i).num_candidates = true;
+  methods(i).color = colormap(i,:);
+  methods(i).is_baseline = false;
+  
+  i = numel(methods) + 1;
+  methods(i).name = 'EdgeBoxes65';
+  methods(i).short_name = 'EB65';
+  prefix = [precomputed_prefix 'edge_boxes_65/'];
+  methods(i).candidate_dir = [prefix 'mat'];
+  methods(i).repeatability_candidate_dir = [prefix 'repeatability_mat'];
+  methods(i).best_voc07_candidates_file = [prefix 'best_candidates.mat'];
+  methods(i).best_imagenet_candidates_file = [prefix 'best_candidates_imagenet.mat'];
+  methods(i).repeatability_matching_file = [prefix 'repeatability_matching.mat'];
+  methods(i).order = 'descend';
+  methods(i).extract = @(im,num) run_edge_boxes(im, num, 0.65, 0.70);
+  methods(i).num_candidates = true;
+  methods(i).color = colormap(i,:);
+  methods(i).is_baseline = false;
+  
+  i = numel(methods) + 1;
+  methods(i).name = 'EdgeBoxes75';
+  methods(i).short_name = 'EB75';
+  prefix = [precomputed_prefix 'edge_boxes_75/'];
+  methods(i).candidate_dir = [prefix 'mat'];
+  methods(i).repeatability_candidate_dir = [prefix 'repeatability_mat'];
+  methods(i).best_voc07_candidates_file = [prefix 'best_candidates.mat'];
+  methods(i).best_imagenet_candidates_file = [prefix 'best_candidates_imagenet.mat'];
+  methods(i).repeatability_matching_file = [prefix 'repeatability_matching.mat'];
+  methods(i).order = 'descend';
+  methods(i).extract = @(im,num) run_edge_boxes(im, num, 0.70, 0.80);
+  methods(i).num_candidates = true;
+  methods(i).color = colormap(i,:);
+  methods(i).is_baseline = false;
+  
+  i = numel(methods) + 1;
+  methods(i).name = 'EdgeBoxes85';
+  methods(i).short_name = 'EB85';
+  prefix = [precomputed_prefix 'edge_boxes_85/'];
+  methods(i).candidate_dir = [prefix 'mat'];
+  methods(i).repeatability_candidate_dir = [prefix 'repeatability_mat'];
+  methods(i).best_voc07_candidates_file = [prefix 'best_candidates.mat'];
+  methods(i).best_imagenet_candidates_file = [prefix 'best_candidates_imagenet.mat'];
+  methods(i).repeatability_matching_file = [prefix 'repeatability_matching.mat'];
+  methods(i).order = 'descend';
+  methods(i).extract = @(im,num) run_edge_boxes(im, num, 0.80, 0.90);
   methods(i).num_candidates = true;
   methods(i).color = colormap(i,:);
   methods(i).is_baseline = false;
