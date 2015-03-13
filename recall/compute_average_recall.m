47e31cba88c5e44e84bc86f6eb01e3c6f2e55839
Jan Hosang <jan.hosang@gmail.com>
2015-03-13 19:59:08 +0100

    bugfix to AR computation, thanks @rbgirshick

--- recall/compute_average_recall.m
+++ recall/compute_average_recall.m
@@ -1,32 +1,15 @@
 function [overlap, recall, AR] = compute_average_recall(unsorted_overlaps)
-  overlap = sort(unsorted_overlaps(:)', 'ascend');
-  num_pos = numel(overlap);
-  if max(overlap) < 1
-    overlap = [0, overlap, max(overlap)+0.001];
-    recall = [1, (num_pos:-1:1)/num_pos, 0];
-  else
-    overlap = [0, overlap];
-    recall = [1, (num_pos:-1:1)/num_pos];
+  all_overlaps = sort(unsorted_overlaps(:)', 'ascend');
+  num_pos = numel(all_overlaps);
+  dx = 0.001;
+
+  overlap = 0:dx:1;
+  overlap(end) = 1;
+  recall = zeros(length(overlap), 1);
+  for i = 1:length(overlap)
+    recall(i) = sum(all_overlaps >= overlap(i)) / num_pos;
   end
-  
-  good_overlap = overlap(overlap >= 0.5);
+
   good_recall = recall(overlap >= 0.5);
-  dx = good_overlap(2:end) - good_overlap(1:end-1);
-  y = (good_recall(1:end-1) + good_recall(2:end)) / 2;
-  AR = 2 * sum(dx .* y);
-  
-  % resample overlap/recall so the plot points are not too dense
-  delta = 0.005;
-  mask = false(size(overlap));
-  next_overlap = delta;
-  for i = 1:(numel(overlap) - 1)
-    if overlap(i+1) >= next_overlap
-      mask(i) = true;
-      next_overlap = overlap(i) + delta;
-    end
-  end
-  mask(1) = true;
-  mask(end) = true;
-  overlap = overlap(mask);
-  recall = recall(mask);
+  AR = 2 * dx * trapz(good_recall);
 end
