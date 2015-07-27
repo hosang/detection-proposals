70497d1fe63bb50c7c23291e21029fab4545061d
Jan Hosang <jan.hosang@gmail.com>
2015-07-27 11:08:14 +0200

    added coco saving of candidates

--- shared/save_candidates_mat.m
+++ shared/save_candidates_mat.m
@@ -1,4 +1,4 @@
-function save_candidates_mat(dirname, img_id, boxes, scores, num_candidates, subdirlen)
+function save_candidates_mat(dirname, img_id, boxes, scores, num_candidates, subdirlen, coco)
 % Save candidates to disk.
 
   if nargin < 5
@@ -7,7 +7,20 @@ function save_candidates_mat(dirname, img_id, boxes, scores, num_candidates, sub
   if nargin < 6
     subdirlen = 4;
   end
-  subdir = img_id(1:subdirlen);
+  if nargin < 7
+    coco = false;
+  end
+    
+  if coco
+    % full directories turn out to be slow
+    subdirlen1 = 14;
+    subdir1 = img_id(1:subdirlen1);
+    subdirlen2 = 22;
+    subdir2 = img_id(1:subdirlen2);
+    subdir = fullfile(subdir1, subdir2);
+  else
+    subdir = img_id(1:subdirlen);
+  end
 
   path = fullfile(dirname, subdir);
   if ~exist(path, 'dir')
